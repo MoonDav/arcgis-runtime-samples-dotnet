ea803db67f556172f45072e8d363a8a14b663bbe
thad <ttilton@esri.com>
2016-08-26 15:29:23 -0700

    Renamed MainPage.xx files in each security sample to match the topic; added ReadMe files

--- src/Windows/ArcGISRuntime.Windows.Samples/Samples/Security/PKIAuthenticationVB/PKIAuthenticationVB.xaml.vb
+++ src/Windows/ArcGISRuntime.Windows.Samples/Samples/Security/PKIAuthenticationVB/PKIAuthenticationVB.xaml.vb
@@ -20,7 +20,7 @@ Public NotInheritable Class MainPage
     Inherits Page
 
     'TODO - Add the URL for your PKI-secured portal
-    Const SecuredPortalUrl As String = "https://portalpkiqa.ags.esri.com/sharing/rest"
+    Const SecuredPortalUrl As String = "https://my.secure.server.com/sharing/rest"
 
     'TODO - Add the URL for a portal containing public content (ArcGIS Organization, e.g.)
     Const PublicPortalUrl As String = "http://esrihax.maps.arcgis.com/sharing/rest"
